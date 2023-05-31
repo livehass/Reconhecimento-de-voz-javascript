6d7454c37e4546f217cddb58ae15c97643acd9bb
livehass <silva.felipe12@hotmail.com>
2023-05-31 20:32:29 -0300

    new else if

--- app/validations.js
+++ app/validations.js
@@ -4,16 +4,22 @@ function verifyIfGuessIsValid(guess){
 
     if(ifGuessIsInvalid(number)){
         elementGuess.innerHTML += ` <div class="error"> Must be a number between ${lowValue} to ${highValue}</div>`
+        
+        return
     }
     if (numberIsbiggerOrLowerThanAllowed(number)) {
         elementGuess.innerHTML +=` <div class="error"> Must be a number between ${lowValue} to ${highValue}</div>`
+
+        return 
     }
     if (number === randomNumber) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
-            <h4>O número secreto era <span id="right-number">${number}!</span></h4>
+            <h4>O número secreto era <span id="right-number">${number}</span></h4>
 
-        `   
+        ` 
+    }else if (number > randomNumber){
+
     }
 }    
 function ifGuessIsInvalid(number) {
