6338cac7f5abcb4384ef545876aedc9709ae1aa0
livehass <silva.felipe12@hotmail.com>
2023-05-31 15:14:40 -0300

    ADD new function recognition

--- app/voice-recognition.js
+++ app/voice-recognition.js
@@ -1,1 +1,13 @@
-var window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+ const elementGuess = document.getElementById('Guess')
+ 
+ 
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br';
+recognition.start();
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+     (e.results[0][0].transcript);
+}
