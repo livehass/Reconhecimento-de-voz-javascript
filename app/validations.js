ede5156650e26258839c88a9c1882430f86e5cfe
livehass <silva.felipe12@hotmail.com>
2023-05-31 17:43:13 -0300

    add new function validadate the number

--- app/validations.js
+++ app/validations.js
@@ -1,3 +1,13 @@
 function verifyIfGuessIsValid(guess){
+    const number = +guess
 
+    if(ifGuessIsInvalid(number)){
+        console.log("invalid")
+    }else {
+        console.log("valid");
+    }
 }
+function ifGuessIsInvalid(number) {
+    return Number.isNaN(number);
+}
+
