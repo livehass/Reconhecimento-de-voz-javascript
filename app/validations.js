d1244c2f7a713924559ee83008a30630f45c1900
livehass <silva.felipe12@hotmail.com>
2023-06-01 00:33:43 -0300

    last commit

--- app/validations.js
+++ app/validations.js
@@ -1,14 +1,13 @@
-
 function verifyIfGuessIsValid(guess){
     const number = +guess
 
     if(ifGuessIsInvalid(number)){
-        elementGuess.innerHTML += ` <div class="error"> Must be a number between ${lowValue} to ${highValue}</div>`
+        elementGuess.innerHTML += ` <div class="error"> o número deve ser entre ${lowValue} até ${highValue}</div>`
         
         return
     }
     if (numberIsbiggerOrLowerThanAllowed(number)) {
-        elementGuess.innerHTML +=` <div class="error"> Must be a number between ${lowValue} to ${highValue}</div>`
+        elementGuess.innerHTML +=` <div class="error"> o número deve ser entre ${lowValue} até ${highValue}</div>`
 
         return 
     }
@@ -16,15 +15,15 @@ function verifyIfGuessIsValid(guess){
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h4>O número secreto era <span id="right-number">${number}</span></h4>
-
         ` 
     }else if (number > randomNumber){
         elementGuess.innerHTML = `
-        <div>O número secreto é maior <i class="fa-solid fa-down-long fa-bounce"></i> </div> 
+            <div>O número secreto é maior <i class="fa-solid fa-down-long fa-bounce"></i> </div> 
+        
         `
     }else {
         elementGuess.innerHTML = `
-        <div>O número secreto é maior <i class="fa-solid fa-up-long fa-bounce"></i> </div> 
+            <div>O número secreto é maior <i class="fa-solid fa-up-long fa-bounce"></i> </div> 
         `
     }
 }    
